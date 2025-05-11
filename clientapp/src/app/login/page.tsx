af30e214d89a3f1a04823bf33e45036be83d628b
Syed Abdullah Hassan <56790074+Abdullah12has@users.noreply.github.com>
2025-05-11 23:29:37 +0200

    frontend fix

---
+++ clientapp/src/app/login/page.tsx
@@ -0,0 +1,20 @@
+// "use client"
+import { LampDemo } from "@/components/lampDemo";
+import { SidebarDemo } from "@/components/sidebar";
+import axios from "axios";
+import { div } from "motion/react-client";
+import Image from "next/image";
+
+export default async function Login() {
+
+
+
+    return (
+        <div className="w-screen h-screen flex flex-col  bg-[#0E0E10]">
+
+
+            <SidebarDemo />
+
+        </div>
+    );
+}
